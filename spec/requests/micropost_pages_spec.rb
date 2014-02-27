b7a6767b7aff0b03b3711648a40219a47a71870b
Ronnakrit Kunaviriyasiri <r.kunaviriyasiri@gmail.com>
2014-02-27 22:37:34 +0700

    Add user microposts

--- spec/requests/micropost_pages_spec.rb
+++ spec/requests/micropost_pages_spec.rb
@@ -6,6 +6,18 @@ describe "MicropostPages" do
   let(:user) { FactoryGirl.create(:user)}
   before { sign_in user }
 
+  describe "micropost destruction" do
+  	before { FactoryGirl.create(:micropost, user: user)}
+
+  	describe "as correct user" do
+  		before { visit root_path }
+
+  		it "should delete a micropost" do
+  			expect { click_link "delete" }.to change(Micropost, :count).by(-1)
+  		end
+  	end
+  end
+
   describe "with invalid information" do
   	it " should not create a micropost" do
   		expect { click_button "Post"}.not_to change(Micropost, :count)
